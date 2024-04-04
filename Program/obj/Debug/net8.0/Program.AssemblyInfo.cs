c05c97ab137f2900a3b8c615706c61112c451313
dimitar-panev <dimiturpanev16@gmail.com>
2024-04-04 15:05:42 +0300

    .

--- Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+016cae92805ee69d73d220e88bc49afed4b344e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc55fd9dad2e8977dcd475ea69934cef51fd73ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
